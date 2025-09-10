94aa89bc37462e9ef662a0959bee01bc0d9a0391
saqibkhan314 <khansaqibkhan314@gmail.com>
2025-09-10 13:54:16 +0530

    done user , captain and gateway

---
+++ captain/routes/captain.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+router.post('/register', captainController.register);
+router.post('/login', captainController.login);
+router.get('/logout', captainController.logout);
+router.get('/profile', authMiddleware.captainAuth, captainController.profile);
+router.patch('/toggle-availability', authMiddleware.captainAuth, captainController.toggleAvailability);
+// router.get('/accepted-ride',authMiddleware.captainAuth, captainController.acceptedRide);
+
+module.exports = router;
